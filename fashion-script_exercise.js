925508a2fffa523e2e462cda9722d4e0216ce344
Coderangshu <57983431+Coderangshu@users.noreply.github.com>
2020-04-29 00:54:21 +0530

    Update

--- fashion-script_exercise.js
+++ fashion-script_exercise.js
@@ -144,7 +144,7 @@ async function run() {
     const model = getModel();
     tfvis.show.modelSummary({name: 'Model Architecture'}, model);
     await train(model, data);
-    await model.save('downloads://my_model');
+    //await model.save('downloads://my_model');
     init();
     alert("Training is done, try classifying your drawings!");
 }
